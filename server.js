063660025fc7492b152c687baff44244a51d90ba
IsmaelRuge1 <ismael.ruge@ithealth.co>
2024-02-16 10:05:00 -0500

    Prueba

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('Nuevo cliente conectado');
+
+    socket.on('mensaje', (data) => {
+        console.log('Mensaje recibido:', data);
+        io.emit('mensaje', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor WebSocket iniciado en el puerto ${PORT}`);
+});
